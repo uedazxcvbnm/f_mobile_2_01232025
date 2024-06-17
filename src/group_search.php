f9f282ab04258503f7b5474ed172a11b5fe6db6a
Atuy0000 <kd1302867@st.kobedenshi.ac.jp>
2024-06-17 11:53:10 +0900

    グループ一覧

---
+++ src/group_search.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <title>禁酒アプリ</title>
+        <link rel="stylesheet" href="../css/group_search.css">
+    </head>
+    <body>
+        <h1>グループ検索一覧</h1>
+        <div class="bt">
+            <button>グループA<br><div class="fsize">参加人数5人</div></button>
+            <button>グループB<br><div class="fsize">参加人数10人</div></button>
+            <button>グループC<br><div class="fsize">参加人数8人</div></button>
+            <button>グループD<br><div class="fsize">参加人数3人</div></button>
+            <button>グループE<br><div class="fsize">参加人数5人</div></button>
+            <button>グループF<br><div class="fsize">参加人数13人</div></button>
+        </div>
+    </body>
+</html>
