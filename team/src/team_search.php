6288278286e7c36c556753ecd17fb0ecbb1c55a6
Atuy0000 <kd1302867@st.kobedenshi.ac.jp>
2024-07-08 10:56:24 +0900

    修正

--- team/src/team_search.php
+++ team/src/team_search.php
@@ -13,7 +13,7 @@ $group = $product->getTeam($ident);
 <html lang="ja">
     <head>
         <title>禁酒アプリ</title>
-        <link rel="stylesheet" href="../css/test.css">
+        <link rel="stylesheet" href="../css/team_search.css">
     </head>
     <body>
         <h1>グループ検索一覧</h1>
