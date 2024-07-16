31981f2257565ddc42e0e81e519b65454588dc5a
uedazxcvbnm <kd1325559@st.kobedenshi.ac.jp>
2024-07-16 14:51:17 +0900

    グラフ画面_ログアウト状態の場合強制的に画面遷移

--- input_daily_record/graph/graph.php
+++ input_daily_record/graph/graph.php
@@ -1,7 +1,10 @@
 <?php
     session_start();
-    $user_id = $_SESSION['user_id'];
-    // echo $user_id;
+    // ログインしていないときの処理
+    if (!isset($_SESSION['user_id'])){
+        header('Location: ./../../login/login_display.php');
+        exit();
+    }
 ?>
 
 <!DOCTYPE html>
