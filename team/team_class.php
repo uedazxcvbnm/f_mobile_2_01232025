6bc782ea22cc3949d7bbb33ec2a6c1ae8b1c0585
Atuy0000 <kd1302867@st.kobedenshi.ac.jp>
2024-10-15 10:13:18 +0900

    DB変更、未参加のグループのみ表示

--- team/team_class.php
+++ team/team_class.php
@@ -3,20 +3,28 @@ require_once __DIR__ . '../../input_daily_record/classes/dbdata.php';
 
 class Team extends dbdata
 {
-    public function getTeams()
+    public function getTeams($ident, $id)
     {
-        $sql = "select * from teams";
-        $stmt = $this->query($sql, []);
+        $sql = "select * from teams right outer join (select * from joined_teams order by user_id = ? desc, team_id asc limit 10000) joined_teams on teams.team_id = joined_teams.team_id group by joined_teams.team_id having not joined_teams.user_id = ?";
+        $stmt = $this->query($sql, [$ident, $id]);
         $teams = $stmt->fetchAll();
         return $teams;
     }
 
     public function getTeam($ident)
     {
-        $sql = "select * from teams where ident = ?";
+        $sql = "select * from teams where team_id = ?";
         $stmt = $this->query($sql, [$ident]);
         $team = $stmt->fetch();
         return $team;
+    }
+
+    public function getNewTeam()
+    {
+        $sql = "select max(team_id) from teams";
+        $stmt = $this->query($sql, []);
+        $new_team = $stmt->fetch();
+        return $new_team;
     }
 
     public function addTeam($name, $detail)
@@ -25,9 +33,15 @@ class Team extends dbdata
         $result = $this->exec($sql, [$name, $detail]);
     }
 
-    public function addUserTeam($name, $rname, $user_id)
+    public function joinedTeam($team_id, $user_id)
     {
-        $sql = "update user set teams = case when teams like '%' ? '%' then teams else concat(teams, ? ',') end where user_id = ?";
-        $result = $this->exec($sql, [$name, $rname, $user_id]);
+        $sql = "insert into joined_teams (team_id, user_id) values(?, ?)";
+        $result = $this->exec($sql, [$team_id, $user_id]);
+    }
+
+    public function addTeamSize($team_id)
+    {
+        $sql = "update teams set size = size+1 where team_id = ?";
+        $result = $this->exec($sql, [$team_id]);
     }
 }
