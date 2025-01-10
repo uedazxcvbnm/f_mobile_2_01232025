c901184fb5563b4a07090bfb3d5d106a1c2d9e13
uedazxcvbnm <kd1325559@st.kobedenshi.ac.jp>
2025-01-10 13:58:14 +0900

    login_execは変更しなくていい

--- login/login_exec.php
+++ login/login_exec.php
@@ -1,6 +1,6 @@
 <?php
 ini_set('session.cookie_httponly', 1);
-ini_set('session.cookie_secure', 0);// HTTPS環境でない場合は 0 に設定
+ini_set('session.cookie_secure', 1);
 ini_set('session.use_strict_mode', 1);
 session_start();
 
